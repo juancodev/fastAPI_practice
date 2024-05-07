cee5173e41aaa5d1c949310c7ed36decdc70acdd
juancodev <montillasanchezjuancarlos@gmail.com>
2024-05-06 20:32:17 -0400

    🐛 Fix problem with empty folder

--- router/home.py
+++ router/home.py
@@ -1,11 +1,12 @@
 from fastapi import Request, APIRouter
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+
+# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 home_router = APIRouter()
 
-home_router.mount("/static", StaticFiles(directory="static"), name="static")
+# home_router.mount("/static", StaticFiles(directory="static"), name="static")
 
 template = Jinja2Templates(directory="template")
 
